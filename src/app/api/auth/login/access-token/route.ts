a652ec0db3c8dbb4a91c75862f7d07c6a1a259de
pbtyt <darkyt9@gmail.com>
2026-09-18 16:13:39 +0300

    - Fixed access-token

--- src/app/api/auth/login/access-token/route.ts
+++ src/app/api/auth/login/access-token/route.ts
@@ -1,20 +1,24 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
+	const cookieStore = await cookies();
+
 	const backendResponse = await fetch(
 		`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login/access-token`,
 		{
 			method: 'POST',
+			headers: {
+				cookie: cookieStore.toString(),
+			},
 		},
 	);
 
 	const data = await backendResponse.json();
-	const setCookie = backendResponse.headers.get('set-cookie');
-
 	const response = NextResponse.json(data, { status: backendResponse.status });
 
-	if (setCookie) {
-		response.headers.set('set-cookie', setCookie);
+	for (const cookie of backendResponse.headers.getSetCookie()) {
+		response.headers.append('set-cookie', cookie);
 	}
 
 	return response;
